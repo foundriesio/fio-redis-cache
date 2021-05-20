fdb9e3289d37ae15ab993716030d98f302d483a2
Milo Casagrande <milo@foundries.io>
2021-05-20 08:46:07 +0200

    fix: set expire in atomic operation
    Signed-off-by: Milo Casagrande <milo@foundries.io>

--- src/index.js
+++ src/index.js
@@ -11,20 +11,19 @@ class Cache {
  * @param {String} key The key.
  * @param {String|Array} data The data to set.
  * @param {Number} expiresIn Time in second to expire the key.
- * @returns {Promise}
+ * @returns {Promise<String>}
  */
-Cache.prototype.set = async function(key, data, expiresIn = 0) {
+Cache.prototype.set = async function (key, data, expiresIn = 0) {
   try {
-    const st = await this.client.setnxAsync(key, data);
-    if (st === 1 && expiresIn > 0) {
-      await this.client.expireAsync(key, expiresIn);
+    if (expiresIn > 0) {
+      return await this.client.setAsync(key, data, 'EX', expiresIn);
     }
-    return st;
+    return await this.client.setAsync(key, data);
   } catch (err) {
-    this.log.error('Error setting cache with key', key);
-    this.log.error(err);
+    this.log.error('Error setting cache with key "%s"', key);
+    this.log.error({ err });
 
-    return null;
+    return void 0;
   }
 };
 
@@ -33,8 +32,9 @@ Cache.prototype.set = async function(key, data, expiresIn = 0) {
  *
  * @param {String} key The key.
  * @param {Object} data The data to set.
+ * @returns {Promise<Number>}
  */
-Cache.prototype.seth = async function(key, data, expiresIn = 0) {
+Cache.prototype.seth = async function (key, data, expiresIn = 0) {
   try {
     const st = await this.client.hsetAsync(key, ...Object.entries(data));
     if (st > 0 && expiresIn > 0) {
@@ -42,74 +42,78 @@ Cache.prototype.seth = async function(key, data, expiresIn = 0) {
     }
     return st;
   } catch (err) {
-    this.log.error('Error setting hash cache with key', key);
-    this.log.error(err);
+    this.log.error('Error setting hash cache with key "%s"', key);
+    this.log.error({ err });
 
-    return null;
+    return void 0;
   }
 };
 
 /**
  * Get the value of a key.
  *
- * @param {String} key The key to retrieve.
- * @returns {Promise} The key value.
+ * @param {String} key - The key to retrieve.
+ * @returns {Promise<Object>}
  */
-Cache.prototype.get = async function(key) {
+Cache.prototype.get = async function (key) {
   try {
     return await this.client.getAsync(key);
   } catch (err) {
-    this.log.error('Error retrieving key', key);
-    this.log.error(err);
+    this.log.error('Error retrieving key "%s"', key);
+    this.log.error({ err });
 
-    return null;
+    return void 0;
   }
 };
 
 /**
  * Get the entire hash value stored at key.
  *
- * @param {String} key The hash to retrieve.
- * @returns {Object} The hash value.
+ * @param {String} key - The hash to retrieve.
+ * @returns {Promise<Object>}
  */
-Cache.prototype.getallh = async function(key) {
+Cache.prototype.getallh = async function (key) {
   try {
     return await this.client.hgetallAsync(key);
   } catch (err) {
-    this.log.error('Error retrieving hash', key);
-    this.log.error(err);
+    this.log.error('Error retrieving hash "%s"', key);
+    this.log.error({ err });
 
-    return null;
+    return void 0;
   }
 };
 
 /**
  * Get a single filed of an hash stored at key.
  *
- * @param {String} key The hash to retrieve.
- * @param {String} field The hash field to retrieve.
- * @returns {String} The field value.
+ * @param {String} key - The hash to retrieve.
+ * @param {String} field - The hash field to retrieve.
+ * @returns {Promise<String>}
  */
-Cache.prototype.geth = async function(key, field) {
+Cache.prototype.geth = async function (key, field) {
   try {
     return await this.client.hgetAsync(key, field);
   } catch (err) {
-    this.log.error(`Error retrieving field ${field} of ${key}`);
-    this.log.error(err);
+    this.log.error('Error retrieving field "%s" of "%s"', field, key);
+    this.log.error({ err });
 
-    return null;
+    return void 0;
   }
 };
 
 /**
  * Delete a key from the cache.
+ * @param {String} key - The key to remove.
+ * @returns {Promise<Number>}
  */
-Cache.prototype.del = async function(key) {
+Cache.prototype.del = async function (key) {
   try {
     return await this.client.delAsync(key);
   } catch (err) {
-    this.log.error(`Error removing key ${key}`);
-    this.log.error(err);
+    this.log.error('Error removing key "%s"', key);
+    this.log.error({ err });
+
+    return void 0;
   }
 };
 
@@ -128,8 +132,10 @@ Cache.prototype.rpush = async function (key, data, expiresIn = 0) {
     }
     return st;
   } catch (err) {
-    this.log.error(`Error appending list data for key '${key}'`);
-    this.log.error(err);
+    this.log.error('Error appending list data for key "%s"', key);
+    this.log.error({ err });
+
+    return void 0;
   }
 };
 
@@ -144,8 +150,10 @@ Cache.prototype.ltrim = async function (key, start, stop) {
   try {
     return await this.client.ltrimAsync(key, start, stop);
   } catch (err) {
-    this.log.error(`Error trimming list data for key '${key}'`);
-    this.log.error(err);
+    this.log.error('Error trimming list data for key "%s"', key);
+    this.log.error({ err });
+
+    return void 0;
   }
 };
 
@@ -154,13 +162,16 @@ Cache.prototype.ltrim = async function (key, start, stop) {
  * @param {String} key - The key of the list to retrieve.
  * @param {Number} [start=0] - The start index in the list.
  * @param {Number} [stop=-1] - The stop index in the list.
+ * @returns {Promise<Array>}
  */
 Cache.prototype.lrange = async function (key, start = 0, stop = -1) {
   try {
     return await this.client.lrangeAsync(key, start, stop);
   } catch (err) {
-    this.log.error(`Error retrieving list for key '${key}'`);
-    this.log.error(err);
+    this.log.error('Error retrieving list for key "%s"', key);
+    this.log.error({ err });
+
+    return void 0;
   }
 };
 
